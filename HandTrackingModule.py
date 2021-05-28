7375c423372615eaa63a5578cd979e2e5045688e
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2021-05-29 01:21:06 +0300

    commit on second branch
    second commit

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -11,7 +11,7 @@ import math
 import numpy as np
 
 class handDetector():
-	def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+	def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5): 
 		self.mode = mode
 		self.maxHands = maxHands
 		self.detectionCon = detectionCon
